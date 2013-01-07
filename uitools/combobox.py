00e6734d422797098620d6fe1ed8bb79d4b023d6
Mike Boers <mboers@keypics.com>
2013-01-07 11:32:48 -0800

    Convert to camelCase

--- uitools/combobox.py
+++ uitools/combobox.py
@@ -1,19 +1,19 @@
-from PyQt4 import QtGui
+from .qt import *
 
 
 class ComboBox(QtGui.QComboBox):
     
     def itemData(self, *args):
-        return self._clean_data(super(ComboBox, self).itemData(*args).toPyObject())
+        return self._cleanData(super(ComboBox, self).itemData(*args).toPyObject())
     
     def currentData(self):
         return self.itemData(self.currentIndex())
     
-    def _clean_data(self, data):
+    def _cleanData(self, data):
         if isinstance(data, dict):
-            return dict(self._clean_data(x) for x in data.iteritems())
+            return dict(self._cleanData(x) for x in data.iteritems())
         if isinstance(data, (tuple, list)):
-            return type(data)(self._clean_data(x) for x in data)
+            return type(data)(self._cleanData(x) for x in data)
         if isinstance(data, QtCore.QString):
             return unicode(data)
         return data
