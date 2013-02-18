9c8f6d89745ba7b8f83a376696fb89e7f153ecf1
Mike Boers <mboers@keypics.com>
2013-02-18 10:24:23 -0800

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='uitools',
+    version='0.1-dev',
+    description='Collection of general tools and utilities for working with and testing Qt tools.',
+    url='https://github.com/westernx/uitools',
+    
+    packages=['uitools'],
+    
+    author='Mike Boers',
+    author_email='uitools@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
