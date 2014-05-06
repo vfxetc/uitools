cada8fce0519c27fc84de078118243b63a1fd998
Mike Boers <github@mikeboers.com>
2014-05-06 11:14:37 -0700

    Even out QtCore.{pyqtSignal.Signal} differences
    This is, perhaps, going too far.

--- uitools/qt.py
+++ uitools/qt.py
@@ -5,17 +5,32 @@ import sys
 __all__ = ['Qt', 'QtCore', 'QtGui']
 
 try:
+
     import PySide
     from PySide import QtCore, QtGui
     sys.modules.setdefault('PyQt4', PySide)
+
+    # Even out some of the API.
+    QtCore.pyqtSignal = QtCore.Signal
+    QtCore.pyqtSlot = QtCore.Slot
+
 except ImportError:
     try:
+
         import PyQt4
         from PyQt4 import QtCore, QtGui
         sys.modules.setdefault('PySide', PySide)
+        
+        # Even out some of the API.
+        QtCore.Signal = QtCore.pyqtSignal
+        QtCore.Slot = QtCore.pyqtSlot
+
     except ImportError:
+
+        # For our tests, so they don't suffer import errors.
         for name in __all__:
             globals().setdefault(name, None)
 
 
+# For the convenience of our own tools.
 Qt = QtCore.Qt if QtCore else None
