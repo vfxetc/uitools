6854f889e38f565acb80c52a74df09730e0f7e45
Mike Boers <westernx@mikeboers.com>
2015-06-08 10:49:59 -0700

    Clean up for production Linux use

--- uitools/notifications/linux.py
+++ uitools/notifications/linux.py
@@ -2,6 +2,8 @@
 from gi.repository import GLib, Notify as LibNotify
 
 
+DEV = False
+
 class Notification(object):
 
     def __init__(self, title, message, subtitle=None, sticky=False):
@@ -16,20 +18,22 @@ class Notification(object):
         # see: https://developer.gnome.org/libnotify/0.7/libnotify-notify.html
         # see: https://developer.gnome.org/libnotify/0.7/NotifyNotification.html
 
-        # Can check LibNotify.get_server_caps() for a list of capabilities.
-        print 'capabilities', LibNotify.get_server_caps()
+        LibNotify.init('com.westernx.uitools.notifications')
         self._notification = notification = LibNotify.Notification.new(
             self.title,
             self.message,
-            'folder-new'
+            'dialog-information'
         )
         
-        # If this is "default", then it is the default action for clicking the notification.
-        notification.add_action('default', 'Default Action', _on_action, 'on_action_payload')
-        notification.add_action('not_default', 'Another Action', _on_action, 'another_payload')
-        notification.connect('closed', _on_action)
+        if DEV:
+            # Can check LibNotify.get_server_caps() for a list of capabilities.
+            print 'capabilities', LibNotify.get_server_caps()
+            # If this is "default", then it is the default action for clicking the notification.
+            notification.add_action('default', 'Default Action', _on_action, 'on_action_payload')
+            notification.add_action('not_default', 'Another Action', _on_action, 'another_payload')
+            notification.connect('closed', _on_action)
+            notification.set_timeout(5000) # 5s
 
-        notification.set_timeout(5000) # 5s
         notification.show()
 
         # NOTE: This object NEEDS to be held onto for the callback to work.
